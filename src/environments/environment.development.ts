5f0e85d5b41bef98e0d4159accc94be4798252b9
Abin Abraham <abin.a@taomish.com>
2024-09-28 18:27:25 +0530

    added firebase and configs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDtAXH8xRdWegmE1z4nL999o5JjwAql2aQ",
+    authDomain: "datbase-5cbe1.firebaseapp.com",
+    databaseURL: "https://datbase-5cbe1.firebaseio.com",
+    projectId: "datbase-5cbe1",
+    storageBucket: "datbase-5cbe1.appspot.com",
+    messagingSenderId: "1037371790010",
+    appId: "1:1037371790010:web:e6912c216e227a133a6883",
+    measurementId: "G-P8434F99DF"
+  }
+};
